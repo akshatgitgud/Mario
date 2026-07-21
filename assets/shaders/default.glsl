0561154e065c6b35c10fce97340292a78e145d6e
Akshat <luffytapri@gmail.com>
2026-07-22 00:06:09 +0530

    Added assets , drew first sqaure and some rendering stuff

---
+++ assets/shaders/default.glsl
@@ -0,0 +1,19 @@
+#type vertex
+#version 330 core
+layout(
+    location = 0) in vec3 aPos; // position and color we will send to the shader
+layout(location = 1) in vec4 aColor;
+
+out vec4 fColor; // will go to fragment shader
+
+void main() {
+  fColor = aColor;
+  gl_Position = vec4(aPos, 1.0);
+}
+
+#type fragment 
+#version 330 core
+
+in vec4 fColor;
+out vec4 color;
+void main() { color = fColor; }
--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -4,9 +4,9 @@ import util.Time;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
-import org.lwjgl.glfw.GLFWVidMode;
+// import org.lwjgl.glfw.GLFWVidMode;
 import java.util.Objects;
-import org.lwjgl.glfw.Callbacks.*;
+// import org.lwjgl.glfw.Callbacks.*;
 
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
@@ -39,11 +39,12 @@ public class Window {
         switch (newScene) {
             case 0:
                 currentScene = new LevelEditorScene();
-                // currentScene.init();
+                currentScene.init();
                 break;
             case 1:
                 currentScene = new LevelScene();
-
+                currentScene.init();
+                break;
             default:
                 assert false : "Unknown scene" + newScene + " ";
                 break;
@@ -108,34 +109,36 @@ public class Window {
     }
 
     public void loop() {
+    float beginTime = Time.getTime();
+    float endTime;
+    float dt = -1.0f;
 
-        while (!glfwWindowShouldClose(glfwWindow)) {
-            float beginTime = Time.getTime();
-            float endTime = Time.getTime();
-            float dt = -1.0f;
-            // Poll events
-            glfwPollEvents();
-            glClearColor(r, g, b, a);
-            glClear(GL_COLOR_BUFFER_BIT);
-            if (dt >= 0){
-                currentScene.update(dt);
-            }
-            // // JUST CHECKING INPUTS
-            if (fadetoblack) {
-                // System.out.println("IS FADING");
-                r = Math.max(r - 0.01f, 0);
-                g = Math.max(g - 0.01f, 0);
-                b = Math.max(b - 0.01f, 0);
-            }
-            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE) && !fadetoblack) {
-                fadetoblack = true;
-                Window.changeScene(1);
-            }
-            glfwSwapBuffers(glfwWindow);
+    while (!glfwWindowShouldClose(glfwWindow)) {
+        // Poll events
+        glfwPollEvents();
+        glClearColor(r, g, b, a);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-            endTime = Time.getTime();
-            dt = endTime - beginTime;
-            beginTime = endTime;
+        if (dt >= 0) {
+            currentScene.update(dt);
         }
+
+        if (fadetoblack) {
+            r = Math.max(r - 0.01f, 0);
+            g = Math.max(g - 0.01f, 0);
+            b = Math.max(b - 0.01f, 0);
+        }
+        if (KeyListener.isKeyPressed(GLFW_KEY_SPACE) && !fadetoblack) {
+            fadetoblack = true;
+            Window.changeScene(1);
+        }
+
+        glfwSwapBuffers(glfwWindow);
+
+        endTime = Time.getTime();
+        dt = endTime - beginTime;
+        beginTime = endTime;
     }
 }
+    }
+
